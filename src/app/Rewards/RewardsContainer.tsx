90e5fc8f5c9c235e35e012f504fa10f22553023f
Harsh Gaur <harsh10009999@gmail.com>
2025-02-01 18:17:46 +0530

    rewards page

---
+++ src/app/Rewards/RewardsContainer.tsx
@@ -0,0 +1,34 @@
+"use client"; 
+
+import React from "react";
+import dynamic from "next/dynamic";
+import Sidebar from "@/app/DashBoard/Sidebar";
+
+// Dynamically import RewardsPage and ProfileSection
+const RewardsPage = dynamic(() => import("./RewardsPage"), { ssr: false });
+const ProfileSection = dynamic(() => import("@/app/DashBoard/ProfileSection"), {
+  ssr: false,
+});
+
+const RewardsContainer = ({ session }: { session: any }) => {
+  return (
+    <div className="flex min-h-screen">
+      {/* Left Sidebar */}
+      <div className="w-72 bg-white border-r border-gray-200 p-1">
+        <Sidebar />
+      </div>
+
+      {/* Main Content (Rewards Page) */}
+      <div className="flex-1 p-6">
+        <RewardsPage />
+      </div>
+
+      {/* Right Profile Section */}
+      <div className="w-72 bg-white border-l border-gray-200 p-4">
+        <ProfileSection session={session} />
+      </div>
+    </div>
+  );
+};
+
+export default RewardsContainer;
